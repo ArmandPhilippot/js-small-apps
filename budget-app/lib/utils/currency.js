1736cbe897088fc778ee507d5a334a854e64d20e
Armand Philippot <git@armandphilippot.com>
2021-09-10 17:15:50 +0200

    chore: add utils functions to format a number based on locale currency

---
+++ budget-app/lib/utils/currency.js
@@ -0,0 +1,47 @@
+/**
+ * Convert a number to fr_FR locale.
+ * @param {Number} number A number to format.
+ * @returns A number formatted with fr_FR locale.
+ */
+const getCurrencyFR = (number) => {
+  const formatted =
+    Number.parseFloat(number)
+      .toFixed(2)
+      .replace(".", ",")
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + " €";
+  return formatted;
+};
+
+/**
+ * Convert a number to en_US locale.
+ * @param {Number} number A number to format.
+ * @returns A number formatted with en_US locale.
+ */
+const getCurrencyUS = (number) => {
+  const formatted =
+    "$" +
+    Number.parseFloat(number)
+      .toFixed(2)
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  return formatted;
+};
+
+/**
+ * Get a number formatted based on a locale.
+ * @param {Number} number A number to format.
+ * @param {String} format A language code.
+ * @returns A formatted number.
+ */
+const getCurrencyFormat = (number, format) => {
+  switch (format) {
+    case "fr_FR":
+      return getCurrencyFR(number);
+    case "en_US":
+      return getCurrencyUS(number);
+    default:
+      console.log("Not supported!");
+      break;
+  }
+};
+
+export default getCurrencyFormat;
