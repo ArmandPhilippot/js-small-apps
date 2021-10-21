173e70b3734c4832fd5d52889278d7aa6d6088c1
Armand Philippot <git@armandphilippot.com>
2021-10-21 22:36:00 +0200

    chore(color-cycle): document the code

--- color-cycle/app.js
+++ color-cycle/app.js
@@ -1,6 +1,11 @@
 let isRunning = false;
 let intervalId;
 
+/**
+ * Check if the provided value is a valid hexadecimal.
+ * @param {String} value - Two hexadecimal symbols.
+ * @returns {Boolean} True if value is a valid hexadecimal ; false otherwise.
+ */
 function isValidHex(value) {
   const hexSymbols = "0123456789ABCDEF";
   const hexArray = value.split("");
@@ -10,6 +15,10 @@ function isValidHex(value) {
   return true;
 }
 
+/**
+ * Print a message to notify user.
+ * @param {String} msg A message to print.
+ */
 function notify(msg) {
   const body = document.querySelector(".body");
   const notification = document.createElement("div");
@@ -22,6 +31,13 @@ function notify(msg) {
   }, 3000);
 }
 
+/**
+ * Check if colors are set and are valid hexadecimal.
+ * @param {String} red - Hexadecimal string for red color.
+ * @param {String} green - Hexadecimal string for green color.
+ * @param {String} blue - Hexadecimal string for blue color.
+ * @returns {Boolean} True if colors are ready; false otherwise.
+ */
 function areColorsReady(red, green, blue) {
   if (!red || !green || !blue) return false;
 
@@ -43,11 +59,19 @@ function areColorsReady(red, green, blue) {
   return true;
 }
 
+/**
+ * Update the preview color with the provided color.
+ * @param {String} color - Hexadecimal string with a leading hash (CSS format).
+ */
 function updatePreviewColor(color) {
   const preview = document.querySelector(".preview");
   preview.style.backgroundColor = color;
 }
 
+/**
+ * Initialize the preview with user settings.
+ * @param {Object} ui - The different element corresponding to the user interface.
+ */
 function setPreview(ui) {
   const red = ui.colors.red.value;
   const green = ui.colors.green.value;
@@ -59,6 +83,11 @@ function setPreview(ui) {
   }
 }
 
+/**
+ * Generate a new color.
+ * @param {Integer} color - An integer corresponding to a RGB value.
+ * @param {Integer} increment - Add this value to color argument.
+ */
 function* getColor(color, increment) {
   let nextColor = color;
   yield nextColor;
@@ -74,10 +103,21 @@ function* getColor(color, increment) {
   }
 }
 
+/**
+ * Convert an RGB color to hexadecimal format.
+ * @param {Integer} red - An integer representing red color with RGB format.
+ * @param {Integer} green - An integer representing green color with RGB format.
+ * @param {Integer} blue - A value representing blue color with RGB format.
+ * @returns {String} The color in hexadecimal format with a leading hash.
+ */
 function getNexHexColor(red, green, blue) {
   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
 
+/**
+ * Disable or enable inputs.
+ * @param {Object} ui - The HTMLElements corresponding to user interface.
+ */
 function toggleInputs(ui) {
   ui.colors.red.disabled = isRunning;
   ui.colors.green.disabled = isRunning;
@@ -88,6 +128,10 @@ function toggleInputs(ui) {
   ui.interval.disabled = isRunning;
 }
 
+/**
+ * Start or stop the preview.
+ * @param {Object} ui - The HTMLElements corresponding to user interface.
+ */
 function start(ui) {
   const red = ui.colors.red.value;
   const green = ui.colors.green.value;
@@ -123,6 +167,10 @@ function start(ui) {
   }
 }
 
+/**
+ * Listen form, buttons and inputs.
+ * @param {Object} ui - The HTMLElements corresponding to user interface.
+ */
 function listen(ui) {
   ui.form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -133,6 +181,9 @@ function listen(ui) {
   ui.colors.blue.addEventListener("change", () => setPreview(ui));
 }
 
+/**
+ * Initialize the app.
+ */
 function init() {
   const ui = {
     button: document.querySelector(".btn"),
